90654581efc189089c5769b31ad98cdf2d221511
Martin Aleksandrov <martin_20.05@abv.bg>
2025-07-15 10:51:07 +0300

    Creating event entity

---
+++ HotelApp/HotelApp/src/main/java/com/HotelApp/HotelApp/entities/Event.java
@@ -0,0 +1,69 @@
+package com.HotelApp.HotelApp.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "event")
+public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    @Column(name = "date", nullable = false)
+    private Date date;
+
+    @Column(name = "location",length = 300)
+    private String location;
+
+    public Event(String name, Date date, String location) {
+        this.name = name;
+        this.date = date;
+        this.location = location;
+    }
+
+    public Event(){}
+
+    //GET & SET Methods
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
--- HotelApp/HotelApp/src/main/java/com/HotelApp/HotelApp/entities/Guest_Event.java
+++ HotelApp/HotelApp/src/main/java/com/HotelApp/HotelApp/entities/Guest_Event.java
@@ -1,4 +1,11 @@
 package com.HotelApp.HotelApp.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "guest_event")
 public class Guest_Event {
+
+
 }
