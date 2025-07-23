cf7af04c4489bcd04a8bcd2e86aed09735353533
Martin Aleksandrov <martin_20.05@abv.bg>
2025-07-23 13:56:54 +0300

    Creating DTO for updating room

---
+++ HotelApp/HotelApp/src/main/java/com/HotelApp/HotelApp/dtos/roomDtos/UpdateRoomDto.java
@@ -0,0 +1,58 @@
+package com.HotelApp.HotelApp.dtos.roomDtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import org.hibernate.validator.constraints.Range;
+import java.math.BigDecimal;
+
+public class UpdateRoomDto {
+
+    @NotBlank(message = "Name is required and must be max 50 symbols!")
+    @Size(max = 50)
+    private String name;
+
+    @NotNull(message = "Size is required")
+    private double size;
+
+    @Range(min = 1, max = 20,message = "Floor must be in range 1-20")
+    private int floor;
+
+    @Positive
+    private BigDecimal price;
+
+
+    //GET & SET
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public int getFloor() {
+        return floor;
+    }
+
+    public void setFloor(int floor) {
+        this.floor = floor;
+    }
+
+    public double getSize() {
+        return size;
+    }
+
+    public void setSize(double size) {
+        this.size = size;
+    }
+}
