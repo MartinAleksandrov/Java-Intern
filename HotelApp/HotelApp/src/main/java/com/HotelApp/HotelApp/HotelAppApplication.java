548c614b32c1a014c24ac237eca5688ce340dd72
Martin Aleksandrov <martin_20.05@abv.bg>
2025-07-23 22:11:55 +0300

    Creating transaction.

--- HotelApp/HotelApp/src/main/java/com/HotelApp/HotelApp/HotelAppApplication.java
+++ HotelApp/HotelApp/src/main/java/com/HotelApp/HotelApp/HotelAppApplication.java
@@ -1,13 +1,26 @@
 package com.HotelApp.HotelApp;
 
+import com.HotelApp.HotelApp.JPQL.JpaUtil;
+import jakarta.persistence.EntityManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.awt.*;
 
 @SpringBootApplication
 public class HotelAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HotelAppApplication.class, args);
-	}
 
+		EntityManager em = JpaUtil.getEntityManager();
+
+		em.getTransaction().begin();
+		System.out.println("Successfully created EntityManager");
+
+		em.getTransaction().commit();
+
+		em.close();
+
+	}
 }
