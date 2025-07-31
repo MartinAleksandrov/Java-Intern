5fcb94837e4493f15c28c9c77eaeee526c00c106
Martin Aleksandrov <martin_20.05@abv.bg>
2025-07-31 14:33:33 +0300

    Different relation between dependecies.

--- Maven-Project/Core/src/main/java/org/example/MyClass.java
+++ Maven-Project/Core/src/main/java/org/example/MyClass.java
@@ -4,7 +4,10 @@ package org.example;
 import lombok.Getter;
 import lombok.Setter;
 
+
 @Getter
 @Setter
 public class MyClass {
+
+
 }
---
+++ Maven-Project/Core/src/test/java/TestClass.java
@@ -0,0 +1,14 @@
+import org.example.MyClass;
+import org.junit.Test;
+import org.mockito.Mock;
+
+public class TestClass {
+
+    @Mock
+    public MyClass myClass;
+
+    @Test
+    public void test() {
+
+    }
+}
