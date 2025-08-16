e5d9120362a256515a55b40561d2e04a840ff428
Martin Aleksandrov <121686622+MartinAleksandrov@users.noreply.github.com>
2025-08-16 11:22:16 +0300

    Creating get room by id and exception handler.

--- HotelApp/HotelApp/src/main/java/com/HotelApp/HotelApp/controllers/RoomController.java
+++ HotelApp/HotelApp/src/main/java/com/HotelApp/HotelApp/controllers/RoomController.java
@@ -1,19 +1,24 @@
 package com.HotelApp.HotelApp.controllers;
 
+import com.HotelApp.HotelApp.GlobalExceptions.RoomErrorResponse;
+import com.HotelApp.HotelApp.GlobalExceptions.RoomNotFoundException;
 import com.HotelApp.HotelApp.dtos.roomDtos.NewRoomDto;
+import com.HotelApp.HotelApp.dtos.roomDtos.RoomDto;
 import com.HotelApp.HotelApp.dtos.roomDtos.UpdateRoomDto;
 import com.HotelApp.HotelApp.services.contracts.RoomService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
 import java.util.UUID;
 
 @RestController
@@ -25,6 +30,26 @@ public class RoomController {
     @Autowired
     public RoomController(RoomService roomService) {
         this.roomService = roomService;
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<RoomDto> getRoom(@PathVariable UUID id) {
+        RoomDto dto =  roomService.getRoomById(id);
+
+        return new ResponseEntity<>(dto,HttpStatus.OK);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<RoomErrorResponse> handleExceptions(RoomNotFoundException ex){
+
+        RoomErrorResponse response = new RoomErrorResponse();
+
+        response.setMessage(ex.getMessage());
+        response.setStatus(HttpStatus.NOT_FOUND.value());
+        response.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(response,HttpStatus.NOT_FOUND);
     }
 
     @PostMapping("/createRoom")
